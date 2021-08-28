99e37d324a6a6e6112e2a3c449198f1a158851fb
SKK <skaiste.ka@gmail.com>
2021-08-28 22:44:01 +0300

    basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import Player from "./components/Player";
+import Song from "./components/Song";
+import Library from "./components/Library";
+import data from "./util";
+
+import './styles/app.scss';
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0
+  });
+  const audioRef = useRef(null);
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({...songInfo, currentTime: current, duration});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song currentSong={currentSong} />
+      <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} audioRef={audioRef} songInfo={songInfo} setSongInfo={setSongInfo} />
+      <Library songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying} audioRef={audioRef} />
+      <audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 }
---
+++ src/components/Library.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LibrarySong from './LibrarySong';
+
+const Library = ({ songs, setCurrentSong, isPlaying, audioRef }) => {
+    return (
+        <div className='library'>
+            <h2>Library</h2>
+            <div className='library-songs'>
+                {songs.map((song) => (
+                    <LibrarySong song={song} setCurrentSong={setCurrentSong} audioRef={audioRef} isPlaying={isPlaying} key={song.id} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Library;
